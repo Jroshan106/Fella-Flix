290d9615f84a79c417e45ad58e46d6d177a95858
Jroshan106 <jroshan456@gmail.com>
2026-09-26 18:55:30 +0530

    Refactor loading componenT

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,64 +1,93 @@
-export default function Loading() {
+﻿export default function Loading() {
   return (
     <div className="flex items-center justify-center min-h-[70vh]">
-      <div className="loader">
-        <div className="inner one"></div>
-        <div className="inner two"></div>
-        <div className="inner three"></div>
+      <div className="sharingon">
+        <div className="ring">
+          <div className="to" />
+          <div className="to" />
+          <div className="to" />
+          <div className="circle" />
+        </div>
       </div>
       <style>{`
-        .loader {
+        .sharingon {
+          position: relative;
+          width: 6em;
+          height: 6em;
+          background-color: red;
+          border: 6px solid black;
+          animation: rot 1s ease-in-out infinite;
+        }
+
+        .ring {
           position: absolute;
-          top: calc(50% - 32px);
-          left: calc(50% - 32px);
-          width: 64px;
-          height: 64px;
+          content: "";
+          left: 50%;
+          top: 50%;
+          width: 3.5em;
+          height: 3.5em;
+          border: 4px solid rgba(110, 13, 13, 0.5);
+          transform: translate(-50%,-50%);
+        }
+
+        .sharingon, .ring, .to, .circle {
           border-radius: 50%;
-          perspective: 800px;
         }
 
-        .inner {
+        .to, .circle {
           position: absolute;
+          content: "";
+          width: 0.9em;
+          height: 0.9em;
+          background-color: black;
+        }
+
+        .to:nth-child(1) {
+          top: -0.5em;
+          left: 50%;
+          transform: translate(-40%);
+        }
+
+        .to::before {
+          content: "";
+          position: absolute;
+          top: -0.5em;
+          right: -0.2em;
+          width: 1.1em;
+          height: 0.9em;
           box-sizing: border-box;
-          width: 100%;
-          height: 100%;
-          border-radius: 50%;  
+          border-left: 16px solid black;
+          border-radius: 100% 0 0;
         }
 
-        .inner.one {
-          left: 0%;
-          top: 0%;
-          animation: rotate-one 1s linear infinite;
-          border-bottom: 3px solid #8fba96;
+        .to:nth-child(2) {
+          bottom: 0.5em;
+          left: -0.35em;
+          transform: rotate(-120deg);
         }
 
-        .inner.two {
-          right: 0%;
-          top: 0%;
-          animation: rotate-two 1s linear infinite;
-          border-right: 3px solid #394d3e;
+        .to:nth-child(3) {
+          bottom: 0.5em;
+          right: -0.35em;
+          transform: rotate(120deg);
         }
 
-        .inner.three {
-          right: 0%;
-          bottom: 0%;
-          animation: rotate-three 1s linear infinite;
-          border-top: 3px solid #8fba96;
+        .circle {
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%,-50%);
+          box-shadow: 0 0 20px 1px;
+          width: 1em;
+          height: 1em;
         }
 
-        @keyframes rotate-one {
-          0% { transform: rotateX(35deg) rotateY(-45deg) rotateZ(0deg); }
-          100% { transform: rotateX(35deg) rotateY(-45deg) rotateZ(360deg); }
-        }
-
-        @keyframes rotate-two {
-          0% { transform: rotateX(50deg) rotateY(10deg) rotateZ(0deg); }
-          100% { transform: rotateX(50deg) rotateY(10deg) rotateZ(360deg); }
-        }
-
-        @keyframes rotate-three {
-          0% { transform: rotateX(35deg) rotateY(55deg) rotateZ(0deg); }
-          100% { transform: rotateX(35deg) rotateY(55deg) rotateZ(360deg); }
+        @keyframes rot {
+          0% {
+            transform: rotate(0deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
         }
       `}</style>
     </div>
